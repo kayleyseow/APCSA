184096483b6c087daaaf48c72f4345c416b6ec14
kayley <47701395+kayleyseow@users.noreply.github.com>
2019-10-23 15:55:01 -0700

    Update Fencepost.java

--- Fencepost.java
+++ Fencepost.java
@@ -1,3 +1,11 @@
+/*Write a program that reads the console for a string and outputs the string with two periods between each letter. Try to make it as efficient as possible.
+
+Sample Input: 
+fencepost
+
+Sample Output:
+f..e..n..c..e..p..o..s..t
+*/
 import java.util.*; 
 public class Fencepost{
 	public static void main(String[] args) {
