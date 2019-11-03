fca4aaf24ca7f0d8855b4382c16f5eafc2577c10
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-03 13:14:59 -0800

    Update GreatestToLeast.java

--- GreatestToLeast.java
+++ GreatestToLeast.java
@@ -2,35 +2,6 @@
 import java.util.*;
 public class GreatestToLeast{
 	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
-		int a = console.nextInt();
-		int b = console.nextInt();
-		int c = console.nextInt();
-		if (a>b) {
-			if(a>c){
-				if (b>c) {
-					System.out.println(a+" "+b+" "+c);
-				}
-				else{
-					System.out.println(a+" "+c+" "+b);
-				}
-			}
-			else{
-				System.out.println(c+" "+a+" "+b);
-			}
-		}
-		else{
-			if (a>c) {
-				System.out.println(b+" "+a+" "+c);	
-			}
-			else{
-				if (b>c) {
-					System.out.println(b+" "+c+" "+a);
-				}
-				else{
-					System.out.println(c+" "+b+" "+a);
-				}
-			}
-		}
+		
 	}
 }
