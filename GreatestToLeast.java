aa7326aa4a9adc5329d6dd1d1ebc82fcbfe074c0
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-03 13:15:13 -0800

    Update GreatestToLeast.java

--- GreatestToLeast.java
+++ GreatestToLeast.java
@@ -2,6 +2,11 @@
 import java.util.*;
 public class GreatestToLeast{
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		int a = console.nextInt();
+		int b = console.nextInt();
+		int c = console.nextInt();
 		
+		}
 	}
 }
