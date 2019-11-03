d9db0862b7c0a1022f2d2fb92b15d40c9dc19d22
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-03 00:52:29 -0700

    Add files via upload
    code to calculate equation

---
+++ Physicsequ.java
@@ -0,0 +1,11 @@
+public class Physicsequ{
+	public static void main(String[] args) {
+		double s0 = 12.0;
+		double v0 = 3.5;
+		double a = 9.8;
+		double t = 10.0;
+		double s = s0 + v0 * t + 0.5 * a * t * t;
+		System.out.println(s);
+	}
+	
+}
