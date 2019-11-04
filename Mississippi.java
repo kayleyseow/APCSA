a3232492ccaf71c369862454688ba5dca1145eb7
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-03 17:44:40 -0800

    Add files via upload

---
+++ Mississippi.java
@@ -0,0 +1,56 @@
+public class Mississippi{
+	public static void letterM(){
+		System.out.println("M     M");
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+		System.out.println("M     M");
+	}
+	public static void letterI(){
+		System.out.println("IIIIIII");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("IIIIIII");
+	}
+	public static void letterS(){
+		System.out.println(" SSSSS");
+		System.out.println("S     S");
+		System.out.println("S");
+		System.out.println(" SSSSS");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+	}
+	public static void letterP(){
+		System.out.println("PPPPPP");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+	}
+	public static void main (String [] args){
+		letterM();
+		System.out.println();
+		letterI();
+		System.out.println();
+		letterS();
+		System.out.println();
+		letterS();
+		System.out.println();
+		letterI();
+		System.out.println();
+		letterP();
+		System.out.println();
+		letterP();
+		System.out.println();
+		letterI();
+		System.out.println();
+	}
+}
