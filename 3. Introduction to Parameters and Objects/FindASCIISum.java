57604f10a42c663340f960a01d7db2e2c1d47d51
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-05 10:27:05 -0800

    Update and rename FindASCIISum2.java to FindASCIISum.java

--- 3. Introduction to Parameters and Objects/FindASCIISum.java
+++ 3. Introduction to Parameters and Objects/FindASCIISum.java
@@ -1,5 +1,12 @@
+/*
+Write a program that uses a scanner to input an integer N followed
+by N strings, then prints the sum of the characters' ASCII values.
+
+Note: Using nextLine() requires that console inputs be separated by
+returns.
+*/
 import java.util.*;
-public class FindASCIISum2{
+public class FindASCIISum{
 	public static void main(String[] args) {
      	Scanner console = new Scanner(System.in);
 		int numberOfString = console.nextInt();
@@ -13,9 +20,9 @@ public class FindASCIISum2{
 	}
 	public static int asciisum(String inputstring){ //int returns an integer value 
 		int cal = 0;
-	    for (int i = 0; i < inputstring.length(); i++){// length is stored as an integer hence its place in the forloop
-	    	cal = cal + (int) inputstring.charAt(i);
-	    }
+	    	for (int i = 0; i < inputstring.length(); i++){// length is stored as an integer hence its place in the forloop
+	    		cal = cal + (int) inputstring.charAt(i);
+	    	}
 		return cal;
 	}
 }
