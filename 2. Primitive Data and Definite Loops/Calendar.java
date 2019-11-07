7047eea9bd551f341dd2b6ce716f57785e01fe9d
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-07 10:43:34 -0800

    Create Calendar.java

---
+++ 2. Primitive Data and Definite Loops/Calendar.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Calendar {
+  public static void main(String args[]) {
+    Scanner console = new Scanner(System.in);
+    System.out.println("Please enter the number of days: ");
+    int days = console.nextInt();
+    System.out.println("Please enter the day of the first Sunday: ");
+    int sunday = console.nextInt();
+    System.out.println("  Sun    Mon    Tue    Wed    Thu    Fri    Sat");
+    for (int i=1; i<=7; i++)
+      System.out.print("+------");
+    System.out.println("+");
+    for (int i=1; i!=sunday; i=sunday) {
+      for (int j=1; j<=8-sunday; j++)
+        System.out.print("|      ");
+      for (int j=1; j<sunday; j++)
+        System.out.print("|   " + j + "  ");
+      System.out.println("|");
+    }
+  int row = (days-sunday+1)/7;
+    for (int i=0; i<row; i++) {
+      for (int j=sunday+i*7; j<sunday+(i+1)*7; j++)
+        System.out.format("|  %2d  ",j);
+      System.out.println("|");
+    }
+    for (int i=row*7+sunday-1; i<days; i+=7) {
+        for (int j=row*7+sunday; j<=days; j++)
+          System.out.format("|  %2d  ",j);
+        for (int j=days+1; j<(row+1)*7+sunday; j++)
+          System.out.print("|      ");
+        System.out.println("|");
+    }
+    for (int i=1; i<=7; i++)
+      System.out.print("+------");
+    System.out.println("+");
+  }
+}
