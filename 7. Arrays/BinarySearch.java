b23f3035bfe1a0003bf2783a78a9704d3a899a76
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-16 10:58:13 -0800

    Update BinarySearch.java

--- 7. Arrays/BinarySearch.java
+++ 7. Arrays/BinarySearch.java
@@ -1,4 +1,23 @@
 import java.util.Scanner;
 public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of terms: ");
+        int numbersinarray = s.nextInt();
+        System.out.println("Enter the terms: ");
+        int arr[] = new int[numbersinarray];
+        for(int i =0;i<numbersinarray;i++) {
+            arr[i] = s.nextInt();
+        }
+        System.out.println("Enter the number you want to search.");
+        int num = s.nextInt();
+        int result = binarySearch(arr,num);
+        if(result!= -1) {
+            System.out.println("The number is at index: "+ result);
+        }
+        else {
+            System.out.println("Number Not Found.");
+        }
+    }
 
 }
