14030fed8f9e3f13dd555d5dc55cf491658a71ff
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-17 00:04:57 -0800

    Update BenfordsLaw.java

--- 6. File Processing/BenfordsLaw.java
+++ 6. File Processing/BenfordsLaw.java
@@ -10,5 +10,31 @@ public class BenfordsLaw {
         int[] digits = countingdigits(inputtedfilename);
         finalresult(digits);//prints out the final answer
     }
+    //method for the sum 
+    public static int sum(int[] number) {
+        int sum = 0;
+        for (int i: number) {//compares the value
+            sum =sum + i;
+        }
+        return sum;//returns something because is not void
+    }
+    //method to count the digits
+    public static int[] countingdigits(Scanner filen) {
+        int[] cn = new int[10];
+        while (filen.hasNext()) {
+            int the1stdigitof = firstdigitof(filen.next());
+            cn[firstdigit(the1stdigitof)]++;
+        }
+        return cn;
+    }
+    //method for the first digit of the number
+    public static int firstdigitof(String digit) {
+        for (char ch : digit.toCharArray()) {
+            if (ch>='1'&& ch <='9') {
+                return ch - '0';
+            }
+        }
+        return 0;
+    }
 
 }
