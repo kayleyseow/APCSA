1780cac893d8bf8fa609df5d7a034c778597c487
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-19 20:30:00 -0800

    Update FenceArea.java

--- 5. Program Logic and Indefinite Loops/FenceArea.java
+++ 5. Program Logic and Indefinite Loops/FenceArea.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class FenceArea{
 	public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int leastx = input.nextInt();
+        int maxy = input.nextInt();
+        int maxx = input.nextInt();
+        int leasty = input.nextInt();
 
     }
 
