d01e1cb5565448541758be3d965d750040c16892
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-21 20:49:26 -0800

    Update HogwartsMurderMystery.java

--- 7. Arrays/HogwartsMurderMystery.java
+++ 7. Arrays/HogwartsMurderMystery.java
@@ -10,6 +10,18 @@ public class HogwartsMurderMystery{
     int first = 0;
     int counter = 0;
     int second = 0;
+    while (console.hasNextInt()){
+      arr[i] = console.nextInt();
+      i++;
+      counter++;
+      if(i == 8){
+          first = arr[0] + arr[3] + arr[5] + arr[6];
+          second = arr[1] + arr[2] + arr[4] + arr[7];
+          System.out.print((char)first);
+          System.out.print((char)second);
+          i = 0;
+      }
+    }
 
   }
 }
