ae55e426a75a8328c238e504d0e569151692a428
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-21 20:48:46 -0800

    Update HogwartsMurderMystery.java

--- 7. Arrays/HogwartsMurderMystery.java
+++ 7. Arrays/HogwartsMurderMystery.java
@@ -3,5 +3,13 @@ import java.util.*;
 import java.io.FileNotFoundException;
 public class HogwartsMurderMystery{
   public static void main(String[] args) throws FileNotFoundException{
+    Scanner console = new Scanner(System.in);
+    int total = console.nextInt();
+        int arr[] = new int[8];
+    int i = 0;
+    int first = 0;
+    int counter = 0;
+    int second = 0;
+
   }
 }
