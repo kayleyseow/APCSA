59e6d0c43854357f2a7ffdf7f9841d456bd5bbf4
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-21 20:48:18 -0800

    Update HogwartsMurderMystery.java

--- 7. Arrays/HogwartsMurderMystery.java
+++ 7. Arrays/HogwartsMurderMystery.java
@@ -2,5 +2,6 @@ import java.io.*;
 import java.util.*;
 import java.io.FileNotFoundException;
 public class HogwartsMurderMystery{
-
+  public static void main(String[] args) throws FileNotFoundException{
+  }
 }
