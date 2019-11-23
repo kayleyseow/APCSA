9ee2732f2224d15339afd0cd26279c043564add5
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-22 22:21:41 -0800

    Update Parentheses.java

--- 4. Conditional Execution/Parentheses.java
+++ 4. Conditional Execution/Parentheses.java
@@ -1,3 +1,24 @@
+/*
+Write a program that takes in a string of parentheses like ()()(((())))() and
+determines if all the parentheses within the string are matched correctly. If
+matched correctly, print the value of the maximum "depth" of the string of
+parentheses.  Do not use arrays.
+
+If you think of an open parenthesis as going one unit down a valley
+and a close parenthesis as going one unit up a valley, the maximum depth of
+the string of the parentheses is the maximum depth you reach as you traverse
+the valley/parentheses string, starting from zero.
+
+Note that (()) is matched correctly, but ))(( is not matched correctly.  In other
+words, a correctly matched parenthesis string is what you would find in a valid
+algebraic expression.
+
+Further examples:
+((()) is not matched.
+()))(( is not matched
+((()((4)))) is matched and has a maximum depth of 4 where the 4 is written.
+((()(((5)))())) is matched and has a maximum depth of 5 where the 5 is written.
+*/
 import java.util.*;
 public class Parentheses{
 	public static void main(String[] args) {
