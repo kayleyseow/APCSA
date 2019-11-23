dae6072e8c88a98d53dfd0d19500ddb359a6b7e8
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-22 22:10:11 -0800

    Update Parentheses.java

--- 4. Conditional Execution/Parentheses.java
+++ 4. Conditional Execution/Parentheses.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class Parentheses{
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter your string below:");
+		String parenthesis= console.next();
+		int length = parenthesis.length();
+		int valley = 0;
+		int highest = 0;
 
-  }
+   	}
 }
