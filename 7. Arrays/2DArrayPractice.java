24be1a05e03a620044fb597a1f868f8dbdc4939b
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-25 00:37:05 -0800

    Update 2DArrayPractice.java

--- 7. Arrays/2DArrayPractice.java
+++ 7. Arrays/2DArrayPractice.java
@@ -11,6 +11,16 @@ public class 2DArrayPractice {
            0 0 0 0
            0 0 0 0 */
         // iterating through the array and setting the value of a cell in the array
+        for (int i = 0; i < twod.length; i++) {
+            for (int j = 0; j < twod[i].length; j++) {
+                twod[i][j] = 5;
+            }
+        }
+        // array looks like this
+        /* 5 5 5 5
+           5 5 5 5
+           5 5 5 5 */
+        // accessing the individual cells of an array
 
     }
 }
