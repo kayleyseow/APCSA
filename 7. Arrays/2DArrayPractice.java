c12f4699616597a8c9c9f4baa1ab550dd86039ed
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-25 00:36:39 -0800

    Update 2DArrayPractice.java

--- 7. Arrays/2DArrayPractice.java
+++ 7. Arrays/2DArrayPractice.java
@@ -1,6 +1,16 @@
 import java.util.*;
 public class 2DArrayPractice {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // how to declare a 2-d array
+        int[][] twod = new int[3][4]; // creates array of 3 rows 4 columns
+        System.out.println(twod.length); // prints 3
+        System.out.println(twod[0].length); // prints 4 – the 0 can be swapped with any int from 0-2 and it would still work
+        // array looks like this
+        /* 0 0 0 0
+           0 0 0 0
+           0 0 0 0 */
+        // iterating through the array and setting the value of a cell in the array
 
     }
 }
