54cf6624fb1b5bf66768937e359a75b79dbb9eea
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-25 00:38:18 -0800

    Update 2DArrayPractice.java

--- 7. Arrays/2DArrayPractice.java
+++ 7. Arrays/2DArrayPractice.java
@@ -21,6 +21,35 @@ public class 2DArrayPractice {
            5 5 5 5
            5 5 5 5 */
         // accessing the individual cells of an array
-
+        System.out.println(twod[2][0]); // prints the element in the third column fist row (5)
+        // assigning array elements by console input
+        for (int i = 0; i < twod.length; i++) {
+            for (int j = 0; j < twod[i].length; j++) {
+                twod[i][j] = input.nextInt();
+            }
+        }
+        // in above example values are assigned first left -> right, then downwards through the array
+        // printing all elements in the first row — swap 0 with any value 0-2
+        for (int i = 0; i < twod[0].length; i++) {
+            System.out.print(twod[0][i] + " ");
+        }
+        System.out.println();
+        // if array looks like this:
+        /* a b c d
+           e f g h
+           k l m n */
+        // then output is:
+        // a b c d
+        // printing all elements in the first column — swap 0 with any value 0-3
+        for (int i = 0; i < twod.length; i++) {
+            System.out.print(twod[i][0] + " ");
+        }
+        System.out.println();
+        // if array looks like this:
+        /* a b c d
+           e f g h
+           k l m n */
+        // then output is:
+        // a e k
     }
 }
