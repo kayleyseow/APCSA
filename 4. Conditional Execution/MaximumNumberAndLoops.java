a8b5a94bc7dd306a754be7730c89f1cae2835ce1
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-26 09:48:43 -0800

    Update MaximumNumberAndLoops.java

--- 4. Conditional Execution/MaximumNumberAndLoops.java
+++ 4. Conditional Execution/MaximumNumberAndLoops.java
@@ -4,7 +4,22 @@ public class MaximumNumberAndLoops {
     Scanner reader = new Scanner(System.in);
     int maxNum = reader.nextInt();
     int loops = reader.nextInt();
-
-
+    //Print top line of bars separated by spaces
+    for (int i = 1;i<=loops;i++){
+      for (int j = 0; j < maxNum; j++) {
+        System.out.print(" ");
+      }
+      System.out.print("|");
+    }
+    System.out.println();
+    //Print 1 to maxNum with 0 at the end once for every loop
+    for (int i = 1;i<=loops;i++){
+      for (int j = 1;j<=maxNum;j++){
+        System.out.print(j);
+        if (j==maxNum){
+          System.out.print(0);
+        }
+      }
+    }
   }
 }
