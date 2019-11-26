7d2ff06f6feca500b693407e22682cf7060cc0b5
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-26 09:47:58 -0800

    Update MaximumNumberAndLoops.java

--- 4. Conditional Execution/MaximumNumberAndLoops.java
+++ 4. Conditional Execution/MaximumNumberAndLoops.java
@@ -1,4 +1,10 @@
 import java.util.Scanner;
 public class MaximumNumberAndLoops {
+   public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int maxNum = reader.nextInt();
+    int loops = reader.nextInt();
 
+
+  }
 }
