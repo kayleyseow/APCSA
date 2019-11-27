7b50b7343bc37c10b0d5e8e81eb1a9deb0fdae3c
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-27 00:01:33 -0800

    Update printArray.java

--- 7. Arrays/printArray.java
+++ 7. Arrays/printArray.java
@@ -1,5 +1,16 @@
 import java.util.*;
 public class printArray{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Please enter number of elements in your array: ");
+		int elementsinarray = console.nextInt();
+		System.out.println("Please enter your array: ");
+		int[] array = new int[elementsinarray];
+        for(int i =0;i<elementsinarray;i++) {
+            array[i] = console.nextInt();
+        }
+        printanarray(array);
+	}
 
 }
 	
