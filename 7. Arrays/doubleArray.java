e07beb4cb06ed39d8ae8d65ae2feabb929203424
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-28 00:24:44 -0800

    Update doubleArray.java

--- 7. Arrays/doubleArray.java
+++ 7. Arrays/doubleArray.java
@@ -1,4 +1,4 @@
-//write method called doublArray which takes in int array and doubles every element in the array
+//write method called doubleArray which takes in int array and doubles every element in the array
 import java.util.*;
 public class doubleArray{
 	public static void main(String[] args) {
