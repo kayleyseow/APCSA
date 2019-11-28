2f869f2cbd32ee0a9f24ed97ee5a45ecd6f8656d
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-28 00:26:15 -0800

    Update doubleArray.java

--- 7. Arrays/doubleArray.java
+++ 7. Arrays/doubleArray.java
@@ -1,5 +1,4 @@
 //write method called doubleArray which takes in int array and doubles every element in the array
-import java.util.*;
 public class doubleArray{
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
@@ -7,6 +6,15 @@ public class doubleArray{
 		int elementsinarray = console.nextInt();
 		System.out.println("Please enter your array: ");
 		int[] array = new int[elementsinarray];
-
+		for(int i =0;i<elementsinarray;i++) {
+			array[i] = console.nextInt();
+		}
+		doubleArray(array);
+	}
+	public static void doubleArray(int[] array){
+		for (int i = 0;i<array.length ;i++ ) {
+			array[i]*=2;
+			System.out.print(array[i]+" ");
+		}
+	}
 }
-	
