a9f19c1e1b737b357eb147a81bc10a1d3a4dedcd
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-29 12:05:28 -0800

    Update leastToGreatestArray.java

--- 7. Arrays/leastToGreatestArray.java
+++ 7. Arrays/leastToGreatestArray.java
@@ -6,9 +6,20 @@ public class leastToGreatestArray{
 		int elementsinarray = console.nextInt();
 		System.out.println("Please enter your array: ");
 		int[] array = new int[elementsinarray];
-        for(int i =0;i<elementsinarray;i++) {
-            array[i] = console.nextInt();
-        }
-
+		for(int i =0;i<elementsinarray;i++) {
+			array[i] = console.nextInt();
+		}
+		leastToGreatestArray(array);
+	}
+	public static void leastToGreatestArray(int[] array){
+		boolean ltog = true;
+		for (int i = 1;i<array.length ;i++ ) {
+			if (array[i]>array[i-1]) {
+			} else {
+				ltog = false;
+				System.out.println(ltog);
+			}
+		}
+		System.out.println(ltog);
+	}
 }
-	
