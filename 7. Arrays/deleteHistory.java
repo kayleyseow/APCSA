1851f3cefa015ce2ea97d5e2b9d3049f36baa64a
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-12-01 13:05:07 -0800

    Update deleteHistory.java

--- 7. Arrays/deleteHistory.java
+++ 7. Arrays/deleteHistory.java
@@ -6,9 +6,13 @@ public class deleteHistory{
 		int elementsinarray = console.nextInt();
 		System.out.println("Please enter your array: ");
 		int[] array = new int[elementsinarray];
-        for(int i =0;i<elementsinarray;i++) {
-            array[i] = console.nextInt();
-        }
+		for(int i =0;i<elementsinarray;i++) {
+			array[i] = console.nextInt();
+		}
+        System.out.println("Please enter number that you want to erase: ");
+        int deletevalue = console.nextInt();
+        DeleteHistorymethod(array,deletevalue);
+	}
 
 }
 	
