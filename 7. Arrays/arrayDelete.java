9ac1fd3985ef803fb6fbfbc84eecbdfef80f3d7e
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-11-30 21:52:45 -0800

    Update arrayDelete.java

--- 7. Arrays/arrayDelete.java
+++ 7. Arrays/arrayDelete.java
@@ -1,4 +1,17 @@
 import java.util.*;
 public class arrayDelete{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Please enter number of elements in your array: ");
+		int elementsinarray = console.nextInt();
+		System.out.println("Please enter your array: ");
+		int[] array = new int[elementsinarray];
+        for(int i =0;i<elementsinarray;i++) {
+            array[i] = console.nextInt();
+        }
+		System.out.println("Please enter index which you want to delete: ");
+		int indexdelete = console.nextInt();
+        arrayDelete(array,indexdelete);
+	}
 
 }	
