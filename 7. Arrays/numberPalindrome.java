a3f3c952e1cc63948f725743284ae8269540b527
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-12-02 11:36:07 -0800

    Update numberPalindrome.java

--- 7. Arrays/numberPalindrome.java
+++ 7. Arrays/numberPalindrome.java
@@ -1,4 +1,18 @@
 import java.util.*;
 public class numberPalindrome{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Please enter number of elements in your array: ");
+		int elementsinarray = console.nextInt();
+		System.out.println("Please enter your array: ");
+		int[] array = new int[elementsinarray];
+        for(int i =0;i<elementsinarray;i++) {
+            array[i] = console.nextInt();
+        }
+        Palindrome(array);
+        for (int i = 0;i<array.length ;i++ ) {
+        	System.out.println(array[i] + " ");
+        }
+	}
 
 }	
