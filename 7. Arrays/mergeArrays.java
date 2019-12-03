180ea9e5100a1e81ffe4c3f5c55c1bf709284063
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-12-03 00:06:48 -0800

    Update mergeArrays.java

--- 7. Arrays/mergeArrays.java
+++ 7. Arrays/mergeArrays.java
@@ -1,4 +1,25 @@
 import java.util.*;
 public class mergeArrays{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Please enter number of elements in your array: ");
+		int elementsinarray = console.nextInt();
+		System.out.println("Please enter your array: ");
+		int[] array = new int[elementsinarray];
+        for(int i =0;i<elementsinarray;i++) {
+            array[i] = console.nextInt();
+        }
+    	System.out.println("Please enter number of elements in your second array: ");
+		int elementsinarray2 = console.nextInt();
+		System.out.println("Please enter your second array: ");
+		int[] array2 = new int[elementsinarray2];
+        for(int i =0;i<elementsinarray;i++) {
+            array2[i] = console.nextInt();
+        }
+        mergeArrays(array, array2);
+        for (int i = 0;i<array.length ;i++ ) {
+        	System.out.println(array[i] + " ");
+        }
+	}
 
 }	
