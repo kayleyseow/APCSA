a5c777d5b5722b8fbd3c5b746feeebd1f444f6fc
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-12-03 00:08:11 -0800

    Update mergeArrays.java

--- 7. Arrays/mergeArrays.java
+++ 7. Arrays/mergeArrays.java
@@ -21,5 +21,25 @@ public class mergeArrays{
 			System.out.println(array[i] + " ");
 		}
 	}
-
+	public static int[] mergeArrays(int[] array, int[] array2){
+		int[] array3 = new int[array.length+array2.length];
+		int counter = 0;
+		for (int i = 0;i<array.length ;i++ ) {
+			array3[counter++] = array[i];
+		}
+		for (int i = 0;i<array2.length ;i++ ) {
+			array3[counter++] = array2[i];
+		}
+		int temp = 0;
+		for (int j = 0;j<array3.length-1 ;j++ ) {
+			for (int i = 1;i<array3.length ;i++ ) {
+				if (array3[i]<array3[i-1]) {
+					temp = array3[i-1];
+					array3[i-1] = array3[i];
+					array3[i]=temp;
+				}
+			}			
+		}
+		return array3;
+	}	
 }	
