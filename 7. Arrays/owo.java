7ae0221b4efd60da099b7e2d021afdc8234380fe
kayley s <47701395+kayleyseow@users.noreply.github.com>
2019-12-04 02:23:46 -0800

    Update owo.java

--- 7. Arrays/owo.java
+++ 7. Arrays/owo.java
@@ -4,5 +4,15 @@ public class owo{
 		boolean[] chararray = new boolean[]{false, false, true, true};
 		System.out.println(owo(chararray));
 	}
-
+	public static char[] owo(boolean[] array){
+		char[] owo = new char [array.length];
+		for (int i = 0;i<array.length ;i++ ) {
+			if (array[i]) {
+				owo[i] = 'o';
+			}else{
+				owo[i]='w';
+			}
+		}
+		return owo;
+	}
 }	
