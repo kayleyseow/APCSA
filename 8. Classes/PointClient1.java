557cf20aa643356c875d4f6d4ab670ba9cb9e155
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-07 11:33:48 -0800

    Update PointClient1.java

--- 8. Classes/PointClient1.java
+++ 8. Classes/PointClient1.java
@@ -1,6 +1,10 @@
 /*
 Point 1
-
+Enhance your Point class to include:
+    A constructor     //that takes in two arguments, Point(x,y);
+	translate method  //produces and returns the translated coordinates (change in x, change in y)
+	getX, getY        //gets the points
+	setXY(x,y)        //sets the points
 */
 public class PointClient{
     public static void main(String[] args){
