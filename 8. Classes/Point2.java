6f24efc6275c3865f26644ec4f0e55484374ab7b
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-09 01:58:12 -0800

    Update Point2.java

--- 8. Classes/Point2.java
+++ 8. Classes/Point2.java
@@ -3,6 +3,11 @@ public class Point{
     // The scope of a field is the entire class
     int x;
     int y;
+    // construct a new point with the given (x,y) location
+    public Point(int initialX, int initialY){
+        x = initialX;
+        y = initialY;
+    }
 
 }
 
