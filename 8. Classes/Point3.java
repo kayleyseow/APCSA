2d23a7c3f1d7a15fcf0001540b23a46a255940c4
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-10 01:23:12 -0800

    Update Point3.java

--- 8. Classes/Point3.java
+++ 8. Classes/Point3.java
@@ -1,3 +1,12 @@
 public class Point {
+    //fields are declared just like variables
+    // The scope of a field is the entire class
+    int x;
+    int y;
+    // construct a new point with the given (x,y) location
+    public Point(int initialX, int initialY){
+        x = initialX;
+        y = initialY;
+    }
 
 }
