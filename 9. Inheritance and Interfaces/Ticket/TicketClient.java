620f69f89bec4646478d181af34ab7f0c5183d54
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-12 01:45:44 -0800

    Update TicketClient.java

--- 9. Inheritance and Interfaces/Ticket/TicketClient.java
+++ 9. Inheritance and Interfaces/Ticket/TicketClient.java
@@ -3,5 +3,10 @@ Client program to exercise your Ticket class
 */
 import java.util.*;
 
+public class TicketClient{
+	public static void main(String[] args){
+
+		Scanner console = new Scanner(System.in);
+
 
 }
