01335bea684d462b3a54191ff4107f4ed45d9ecb
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-12 01:46:07 -0800

    Update TicketClient.java

--- 9. Inheritance and Interfaces/Ticket/TicketClient.java
+++ 9. Inheritance and Interfaces/Ticket/TicketClient.java
@@ -8,5 +8,23 @@ public class TicketClient{
 
 		Scanner console = new Scanner(System.in);
 
+		int num1 = getNumber("Enter ticket number : ", console);
+		WalkupTicket wt1 = new WalkupTicket(num1);
+//		System.out.println("Walkup ticket number " + num1 + ": $" + wt1.getPrice());
+		System.out.println(wt1);
+		System.out.println();
 
-}
+		int num2a = getNumber("Advance ticket ticket number : ", console);
+		int num2b = getNumber("Days in advance : ", console);
+		AdvanceTicket at1 = new AdvanceTicket(num2a, num2b);
+//		System.out.println("Advance ticket number " + num2a + ": $" + at1.getPrice());
+		System.out.println(at1);
+		System.out.println();
+
+		int num3a = getNumber("Student advance ticket ticket number : ", console);
+		int num3b = getNumber("Days in advance : ", console);
+		StudentAdvanceTicket sat1 = new StudentAdvanceTicket(num3a, num3b);
+//		System.out.println("Student advance ticket number " + num3a + ": $" + sat1.getPrice());
+		System.out.println(sat1);
+		System.out.println();
+	}
