667cb32592a15765b1b44002e24e3b03e0bff0f8
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-13 21:07:26 -0800

    Update checkCycles.java

--- 7. Arrays/checkCycles.java
+++ 7. Arrays/checkCycles.java
@@ -6,15 +6,26 @@ public class checkCycles{
 		int elements = console.nextInt();
 		System.out.println("Please enter your array: ");
 		int[] array = new int[elements];
-        for(int i =0;i<elements;i++) {
-            array[i] = console.nextInt();
-        }	
-        System.out.println("Please enter an integer cycle length: ");
-        int cyclelength = console.nextInt();
-        System.out.println(checkCycles(array,cyclelength));
+        	for(int i =0;i<elements;i++) {
+           		array[i] = console.nextInt();
+        	}	
+        	System.out.println("Please enter an integer cycle length: ");
+        	int cyclelength = console.nextInt();
+        	System.out.println(checkCycles(array,cyclelength));
 	}
 	public static boolean checkCycles(int[] array, int n){
 		int next = n;
-
+		for (int i = 0;i<array.length ;i++ ) {
+			if (next>=array.length||next<0) {
+				return false;
+			}
+			else{
+				next = array[next];
+			}
+			if (next==n) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
