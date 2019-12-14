f5cd763fc0eaa098808bf7e6d75f1e2af6deb69a
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-14 12:03:01 -0800

    Update WalkupTicket.java

--- 9. Inheritance and Interfaces/Ticket/WalkupTicket.java
+++ 9. Inheritance and Interfaces/Ticket/WalkupTicket.java
@@ -3,5 +3,12 @@ public class WalkupTicket extends Ticket{
 	public WalkupTicket(int number){
 		super(number);
 	}
+	public double getPrice(){
+		return wtprice;
+	}
 
+	public String toString(){
+		//return Double.toString(wtprice);
+		return "Number: " +super.getNumber()+ ", "+"Price: " + Double.toString(wtprice);
+	}
 }
