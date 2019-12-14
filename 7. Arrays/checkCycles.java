19cf957d70c7348ba9d3d598e25d98ee6b822539
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-13 21:06:03 -0800

    Update checkCycles.java

--- 7. Arrays/checkCycles.java
+++ 7. Arrays/checkCycles.java
@@ -1,6 +1,11 @@
 import java.util.*;
 public class checkCycles{
 	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Please enter the number of elements in array");
+		int elements = console.nextInt();
+		System.out.println("Please enter your array: ");
+		int[] array = new int[elements];
 
 	}
 }
