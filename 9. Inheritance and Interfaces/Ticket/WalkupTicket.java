852197cbb009f47f29805e3565ca928f074df448
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-14 12:03:57 -0800

    Update WalkupTicket.java

--- 9. Inheritance and Interfaces/Ticket/WalkupTicket.java
+++ 9. Inheritance and Interfaces/Ticket/WalkupTicket.java
@@ -1,3 +1,7 @@
+/*
+Implement class WalkupTicket to represent a walk-up event ticket.
+Walk-up tickets are also constructed by number, and they have a price of $50
+*/
 public class WalkupTicket extends Ticket{
 	private int wtprice = 50;
 	public WalkupTicket(int number){
@@ -6,9 +10,7 @@ public class WalkupTicket extends Ticket{
 	public double getPrice(){
 		return wtprice;
 	}
-
 	public String toString(){
-		//return Double.toString(wtprice);
 		return "Number: " +super.getNumber()+ ", "+"Price: " + Double.toString(wtprice);
 	}
 }
