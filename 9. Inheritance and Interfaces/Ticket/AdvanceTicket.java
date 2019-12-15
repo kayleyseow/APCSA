a60fa175265f1401f0dc81e45d65f173fc97c205
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-15 01:36:43 -0800

    Update AdvanceTicket.java

--- 9. Inheritance and Interfaces/Ticket/AdvanceTicket.java
+++ 9. Inheritance and Interfaces/Ticket/AdvanceTicket.java
@@ -1,3 +1,10 @@
+/*
+Implement class AdvanceTicket to represent tickets purchased in advance.
+An advance ticket is constructed with a ticket number and with the number of days
+in advance that the ticket was purchased. Advance tickets purchased 10 or more
+days before the event cost $30, and advance tickets purchased fewer than 10 days
+before the event cost $40.
+*/
 public class AdvanceTicket extends Ticket{
     double atprice;
     int dia;
