29419b68e40893ff099b9611c9f4ecbc92403175
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-15 01:36:00 -0800

    Update AdvanceTicket.java

--- 9. Inheritance and Interfaces/Ticket/AdvanceTicket.java
+++ 9. Inheritance and Interfaces/Ticket/AdvanceTicket.java
@@ -1,5 +1,23 @@
 public class AdvanceTicket extends Ticket{
     double atprice;
     int dia;
-
+    public AdvanceTicket(int number,int days){
+    	super(number);
+        this.dia = days;
+        if(days>=10){
+            this.atprice = 30;
+        }
+        else{
+            this.atprice = 40;
+        }
+    }
+    public int getNumber(){
+        return this.dia;
+    }
+    public double getPrice(){
+        return this.atprice;
+    }
+    public String toString(){
+    	return "Number: " +super.getNumber()+ ", "+"Price: " + Double.toString(this.atprice);
+    }
 }
