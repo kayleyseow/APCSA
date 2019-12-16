a7fb3f861f055dd7450a6f8be5c01c465926c734
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-16 00:29:23 -0800

    Update GFEHClient.java

--- 9. Inheritance and Interfaces/GFEH Class E/GFEHClient.java
+++ 9. Inheritance and Interfaces/GFEH Class E/GFEHClient.java
@@ -4,3 +4,14 @@ method1() call from class E.:
 	When called from class E, output is G1.
 	When called from class H, output is H1.
 */
+public class GFEHClient{
+	public static void main(String[] args){
+		G[] elements = {new G(), new F(), new E(), new H()};
+		for (int i = 0; i < elements.length; i++){
+			System.out.println(elements[i]);
+			elements[i].method1();
+			elements[i].method2();
+			System.out.println();
+		}
+	}
+}
