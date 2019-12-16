22263e03de38ff5fb1d43ea7a2a8b52513e00158
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-16 00:31:11 -0800

    Update G.java

--- 9. Inheritance and Interfaces/GFEH Class E/G.java
+++ 9. Inheritance and Interfaces/GFEH Class E/G.java
@@ -6,6 +6,10 @@ public class G{
 	public void method1(){
 		System.out.println("G1");
 	}
-
+	public void method2(){
+		System.out.println("G2");
+	}
+	public String toString(){
+		return "G";
 	}
 }
