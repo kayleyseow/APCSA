7ac7c7c61fa3650186af6aa11c9c82c4b57ad07a
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-18 18:12:47 -0800

    Update SuperheroClient.java

--- 9. Inheritance and Interfaces/Superhero/SuperheroClient.java
+++ 9. Inheritance and Interfaces/Superhero/SuperheroClient.java
@@ -14,6 +14,24 @@ public class SuperheroClient{
 		Batman batman = new Superhero();
 		batman.punch(batman);
 
+		// System.out.println("slide 20");
+		// Superhero superhero = new Batman();
+		// superhero.punch( (Batman) superhero);
 
+		// System.out.println("slide 22");
+		// Batman batman = new Batman();
+		// ((Superhero) batman).punch(batman);
+
+		// System.out.println("slide 24");
+		// Superhero superhero = new Superhero();
+		// superhero.punch( (Batman) superhero);
+
+		// System.out.println("slide 26");
+		// Superhero superhero = new Batman();
+		// superhero.punch( (Batman) superhero);
+
+		// System.out.println("slide 28");
+		// Batman batman = new Batman();			//note change in Superhero class
+		// ( (Superhero) batman).punch(batman);
 	}
 }
