372ea0aa96f18e7d7453912b6b16ca7076a17249
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-18 18:12:07 -0800

    Update SuperheroClient.java

--- 9. Inheritance and Interfaces/Superhero/SuperheroClient.java
+++ 9. Inheritance and Interfaces/Superhero/SuperheroClient.java
@@ -2,5 +2,10 @@ public class SuperheroClient{
 	public static void main(String[] args) {
 		System.out.println();
 
+		System.out.println("slide 14");
+		Superhero superhero = new Superhero();
+		superhero.punch(superhero);
+
+
 	}
 }
