6ebe348f152360084a9f17cef1b872a0f9ee5259
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2019-12-19 00:31:07 -0800

    Update Superhero.java

--- 9. Inheritance and Interfaces/Superhero/Superhero.java
+++ 9. Inheritance and Interfaces/Superhero/Superhero.java
@@ -8,5 +8,7 @@ public class Superhero{
 		System.out.println("Punch! Punch!");
 	}
 	public void punch(Superhero a){		//comment out for slide 28
+	System.out.println("BOOM " + s); 	//comment out for slide 28
+	}									//comment out for slide 28
 
 }
