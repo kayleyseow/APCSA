fc1f096c88bb2795a00d7c4648a79ddf45f18cd4
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-01-21 11:15:39 -0800

    Update removeDuplicates.java

--- 10. ArrayLists/removeDuplicates.java
+++ 10. ArrayLists/removeDuplicates.java
@@ -1,3 +1,18 @@
+/*
+Write a class called RemoveDuplicates that takes a sorted ArrayList of strings and eliminates
+any duplicates from the list. For example, the list
+[be, be, is, not, or, the, to, to] should be converted to [be, is, not, or, the, to].
+Your method to test for a duplicate should be case insensitive, e.g.
+[be, Be, twice] should be converted to [be, twice], and
+[Be, be, twice] should be converted to [Be, twice].
+Note that because the values will be sorted, all of the duplicates will be grouped together.
+Assume that the ArrayList contains only String values, but keep in mind that it might be empty.
+Have the user input strings from the console to populate your ArrayList.  Assume that the user
+enters only String values, and that the String values are entered in correct alphabetical order.
+Your code need not be robust.  The end of user input should be signified by the user entering
+the String value "lastString" followed by a return, e.g. a complete line of user input might
+look like:  a b c c d lastString, signifying that the starting array is [a, b, c, c, d].
+*/
 import java.util.*;
 public class removeDuplicates{
     public static void main(String[] args){
