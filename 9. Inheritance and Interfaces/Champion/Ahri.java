ba4a0245d2e4f99eca24f1de725701f93ca43c95
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-01-24 13:40:20 -0800

    Add files via upload

---
+++ 9. Inheritance and Interfaces/Champion/Ahri.java
@@ -0,0 +1,10 @@
+public class Ahri extends Champion{
+	String idk;
+	public Ahri(int level, String power, String idk){
+		super(level,power);
+		this.idk = idk;
+	}
+	public void feed(){
+		System.out.println("mid or feed");
+	}
+}
