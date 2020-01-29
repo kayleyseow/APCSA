473d20889bd3424f82ea8ceea09cc509bd31b637
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-01-29 00:09:36 -0800

    Update Person.java

--- 10. ArrayLists/People Person/First Try/Person.java
+++ 10. ArrayLists/People Person/First Try/Person.java
@@ -11,6 +11,13 @@ public class Person{
 	public String getname() {return this.name;}
 	public int getage() {return this.age;}
 	public double getweight() {return this.weight;}
+	/*public String printthing(ArrayList<String> namearray, ArrayList<Integer> agearray, ArrayList<Double> weightarray){
+		String finalstring = "";
+		for (int i = 0;i<agearray.size();i++ ) {
+			finalstring += namearray.get(i)+" "+agearray.get(i)+" "+weightarray.get(i)+"\n";
+		}
+		return finalstring;
+	}*/
 	public String printinfo(){
 		String finalstring = "";
 		finalstring = this.name+" "+this.age+" "+this.weight+"\n";
