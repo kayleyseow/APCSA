5b8e7cc17c3d80a27043668ea00ef3a2cc53a23a
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-02-20 09:30:30 -0800

    Add files via upload

---
+++ 12. Recursion/WriteBinary.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class WriteBinary{
+	public static void main(String[] args) {
+		Scanner console  = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		int integer = console.nextInt();
+		decitobin(integer);
+		System.out.println();
+	}
+	public static void decitobin(int n){
+		if (n == 0) {
+			System.out.print(0);
+		}
+		else{
+			if (n<0) {
+				System.out.print("-");
+			}
+			decimaltobinary(Math.abs(n));
+		}
+	}
+	public static void decimaltobinary(int n){
+		if (n>0) {
+			decimaltobinary(n/2);
+			System.out.print(n%2);
+		}
+	}
+}
