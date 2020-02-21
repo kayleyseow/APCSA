14facf740b91d403332155ef068e79c678c6da06
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-02-21 13:58:02 -0800

    Update WriteBinaryString.java

--- 12. Recursion/WriteBinaryString.java
+++ 12. Recursion/WriteBinaryString.java
@@ -1,3 +1,11 @@
+/*
+Write a second recursive program called WriteBinaryString that takes an integer as a parameter
+and returns the binary expression of the integer in String format For example, WriteBinaryString(44)
+should return 101100. You should print out the binary expression to the console. Your program should
+accommodate a negative integer as input.
+Note: Integer.toString(int a) is a built-in method in Java which returns a String representing the integer
+parameter.
+*/
 import java.util.*;
 public class WriteBinaryString{
 	public static void main(String[] args) {
