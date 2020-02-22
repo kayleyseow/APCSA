0f78c0db2dceaa160945914616bf1a18bc28759e
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-02-22 09:59:55 -0800

    Create WriteBinaryEfficient.java

---
+++ 12. Recursion/WriteBinaryEfficient.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class BinaryTest {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer: " + bin(input.nextInt()));
+	}
+	public static String bin(int n) {
+		if (n == 0) return "0";
+		if (n == 1) return "1";
+		return (n < 0 ? "-" + bin(-1 * n) : bin(n / 2) + (n % 2));
+	}
+}
