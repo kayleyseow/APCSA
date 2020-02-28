e67b6ac1aebf4fa73360db692c750eec8e627a87
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-02-27 19:49:14 -0800

    Add files via upload

---
+++ 12. Recursion/moveDisksAKAtowerofhanoi.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class moveDisksAKAtowerofhanoi{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.print("Enter number of disks: Enter destination post: Moves:\n");
+		int disks = console.nextInt();
+		char destpost = console.next().charAt(0);
+		char temppost;
+		if (destpost=='B') {
+			temppost = 'C';
+		}
+		else temppost = 'B';
+		moveDisks(disks, 'A', destpost, temppost, 0);
+		System.out.println("Number of moves: "+numberofmoves(disks));
+		
+	}
+	public static void moveDisks(int n, char sourcerod, char destinationrod, char temporaryrod, int counter) { 
+		if (n == 1) { 
+			System.out.println(sourcerod + " -> " + destinationrod); 
+			return; 
+		} 
+		else{
+			moveDisks(n-1, sourcerod, temporaryrod, destinationrod, counter++);
+			System.out.println(sourcerod + " -> " + destinationrod); 
+			moveDisks(n-1, temporaryrod, destinationrod, sourcerod, counter++); 
+		}
+	} 
+	public static int numberofmoves(int n) {
+	    if (n == 0) return 0;
+	    return 2 * numberofmoves(n - 1) + 1;
+	}
+}
