8d329fcbae9543fd8333348005eceb814a37abd9
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-03-07 14:12:26 -0800

    Update BackTrackTravel.java

--- 12. Recursion/BackTrackTravel.java
+++ 12. Recursion/BackTrackTravel.java
@@ -1,3 +1,22 @@
+/*
+Write a program to find all the paths from the origin to a given point (x, y).
+You may travel only one step at a time in the positive N, E, or NE direction.
+To satisy the HG test cases, set up your recursion to explore in this order:
+N first, then NE, and finally E. Report each path and the total number of paths.
+
+Example
+input:
+1 2
+output:
+moves: N N E
+moves: N NE
+moves: N E N
+moves: NE N
+moves: E N N
+Number of paths = 5
+Note: The number of paths increases rapidly with distance from origin.  HyperGrade may 
+truncate (and fail the test case) for 4 4.
+*/
 import java.util.*;
 public class BackTrackTravel{
     public static int counter = 0;
