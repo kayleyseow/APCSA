e3f742f00a74339884c157fe3e90c8fc6069764e
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-03-09 10:35:37 -0700

    Create countHi2.java

---
+++ 12. Recursion/More Recursion Practice/countHi2.java
@@ -0,0 +1,29 @@
+/*
+Given a string, compute recursively the number of times lowercase "hi" appears in the 
+string, however do not count "hi" that have an 'x' immedately before them.
+
+countHi2("ahixhi") → 1
+countHi2("ahibhi") → 2
+countHi2("xhixhi") → 0*/
+import java.util.*;
+public class countHi2{
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter String: ");
+		System.out.println(countHi2(console.next()));
+	}
+	public static int countHi2(String str) {
+		if (str.length()<2 )  return 0;
+		if (str.length()==2){
+			if (str.equals("hi")) return 1;
+			return 0;
+		}
+		if(str.charAt(0) == 'x'){
+			if(str.substring(1, 3).equals("hi")) return countHi2(str.substring(3));
+			return countHi2(str.substring(1));
+		}
+		if(str.substring(0, 2).equals("hi"))return 1 + countHi2(str.substring(2));
+		if(str.substring(1, 3).equals("hi"))return 1 + countHi2(str.substring(3));
+		return countHi2(str.substring(2));
+	}
+}
