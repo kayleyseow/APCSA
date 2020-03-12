6c484db0883162fa4c1e8ea6573c3b46e42be1e2
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-03-12 11:10:15 -0700

    Update printSquares.java

--- 12. Recursion/printSquares.java
+++ 12. Recursion/printSquares.java
@@ -1,4 +1,23 @@
-import java.util.*;
+/*
+Implement a recursive method called printSquares to find all the ways to express an integer
+as the sum of squares of unique positive integers.   For example, the call printSquares(200)
+should produce the following output:
+
+positive integer: 200
+unique bases: [1, 2, 3, 4, 5, 8, 9]
+unique bases: [1, 2, 3, 4, 7, 11]
+unique bases: [1, 2, 5, 7, 11]
+unique bases: [1, 3, 4, 5, 6, 7, 8]
+unique bases: [1, 3, 4, 5, 7, 10]
+unique bases: [2, 4, 6, 12]
+unique bases: [2, 14]
+unique bases: [3, 5, 6, 7, 9]
+unique bases: [6, 8, 10]
+
+For a given solution, the list of bases should be in ascending order as in the example.
+For numbers such as 32 and 128, which cannot be represented as a sum of unique squares,
+your method should produce no output.
+*/import java.util.*;
 public class printSquares{
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
