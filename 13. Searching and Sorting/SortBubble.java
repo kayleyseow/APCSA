99f47c69c2752d84ba2f1fa5dfa2371fea053a94
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-03-16 23:26:55 -0700

    Update SortBubble.java

--- 13. Searching and Sorting/SortBubble.java
+++ 13. Searching and Sorting/SortBubble.java
@@ -1,3 +1,31 @@
+/*
+Implement bubbleSort.
+
+The user will supply 10 space-separated integers from the console.
+HyperGrade examples are formatted using Arrays.toString(), but you
+are free to format "by hand." Print each iteration of each sort as
+in the HyperGrade example shown below. With 10 elements in
+your input array, you should print 10 lines of output, including the
+original array.
+
+Example:
+
+input
+9 8 7 6 5 4 3 2 1 0
+
+output:
+bubbleSort
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+[8, 7, 6, 5, 4, 3, 2, 1, 0, 9]
+[7, 6, 5, 4, 3, 2, 1, 0, 8, 9]
+[6, 5, 4, 3, 2, 1, 0, 7, 8, 9]
+[5, 4, 3, 2, 1, 0, 6, 7, 8, 9]
+[4, 3, 2, 1, 0, 5, 6, 7, 8, 9]
+[3, 2, 1, 0, 4, 5, 6, 7, 8, 9]
+[2, 1, 0, 3, 4, 5, 6, 7, 8, 9]
+[1, 0, 2, 3, 4, 5, 6, 7, 8, 9]
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
 import java.util.*;
 public class SortBubble{
 	public static void main(String[] args) {
@@ -6,7 +34,7 @@ public class SortBubble{
 		int[] array = new int[10];
 		for (int i = 0;i<array.length ;i++ ) array[i]=console.nextInt();
 		System.out.println(Arrays.toString(array));
-    int size = array.length;
+    		int size = array.length;
 		for (int i = 0; i < size - 1; i++){
 		    for (int j = 0; j < size - i - 1; j++){
 		        if (array[j] > array[j + 1]){
