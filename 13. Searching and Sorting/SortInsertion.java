8a933eef30380f1c152834f8e24a319779f28317
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-03-18 00:38:50 -0700

    Update SortInsertion.java

--- 13. Searching and Sorting/SortInsertion.java
+++ 13. Searching and Sorting/SortInsertion.java
@@ -1,3 +1,30 @@
+/*
+Implement insertionSort.
+
+The user will supply 10 space-separated integers from the console.
+HyperGrade examples are formatted using Arrays.toString(), but you
+are free to format "by hand." Print each iteration of each sort as
+in the HyperGrade example shown below. With 10 elements in
+your input array, you should print 10 lines of output, including the
+original array.
+
+Example:
+input:
+9 8 7 6 5 4 3 2 1 0
+
+output:
+insertionSort
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+[8, 9, 7, 6, 5, 4, 3, 2, 1, 0]
+[7, 8, 9, 6, 5, 4, 3, 2, 1, 0]
+[6, 7, 8, 9, 5, 4, 3, 2, 1, 0]
+[5, 6, 7, 8, 9, 4, 3, 2, 1, 0]
+[4, 5, 6, 7, 8, 9, 3, 2, 1, 0]
+[3, 4, 5, 6, 7, 8, 9, 2, 1, 0]
+[2, 3, 4, 5, 6, 7, 8, 9, 1, 0]
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
 import java.util.*;
 public class SortInsertion{
 	public static void main(String[] args) {
