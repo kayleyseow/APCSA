4b4cd680e77dcee8784f782b67fd11be59a2b4f5
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-05-17 14:28:27 -0700

    Rename//Update

--- 06. File Processing/Count Words/CountWords.java
+++ 06. File Processing/Count Words/CountWords.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 public class CountWords {
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner input = new Scanner(new File("hamlet.txt")); 
+		Scanner input = new Scanner(new File("Hamlet.txt")); 
 		int count = 0;
 		while (input.hasNext()) { 
 			String word = input.next(); 
