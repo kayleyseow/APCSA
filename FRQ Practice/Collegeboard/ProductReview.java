f6fa9e2c16a5adcd65b94f75e8d6b38e50143710
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-06-14 15:09:15 -0700

    Create ProductReview.java

---
+++ FRQ Practice/Collegeboard/ProductReview.java
@@ -0,0 +1,18 @@
+//from an APCS practice test, to be used with ReviewCollector.java
+public class ProductReview{
+	private String name;
+	private String review;
+	/** Constructs a ProductReview object and initializes the instance variables. */
+	public ProductReview(String pName, String pReview){
+	name = pName;
+	review = pReview;
+	}
+	/** Returns the name of the product. */
+	public String getName(){ 
+		return name; 
+	}
+	/** Returns the review of the product. */
+	public String getReview(){ 
+		return review; 
+	}
+}
