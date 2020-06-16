f5582b92b36b65fd45d68ddfe0f109c2cc773db1
Kayley Seow <47701395+kayleyseow@users.noreply.github.com>
2020-06-16 10:40:52 -0700

    Update Phrase.java

--- FRQ Practice/Collegeboard/Phrase.java
+++ FRQ Practice/Collegeboard/Phrase.java
@@ -12,18 +12,18 @@ public class Phrase{
 	public Phrase(String p){
 		currentPhrase = p;
 	}
-	  /** Retuns the index of the nth occurrence of str in the current phrase;
+	/** Retuns the index of the nth occurrence of str in the current phrase;
   	* returns -1 if the nth occurrence does not exist.
   	* Precondition: str.length() > 0 and n > 0
-		* Postcondition: the current phrase is not modified.
-		*/
-  public int findNthOccurrence(String str, int n){
-    /*implementation not shown*/
-  }
-  /** Modifies the current phrase by replacing the nth occurrence of str with repl.
-  * If the nth occurrence does not exist, the current phrase is unchanged.
-  * Precondition: str.length() > 0 and N > 0
-  */
+	* Postcondition: the current phrase is not modified.
+	*/
+	public int findNthOccurrence(String str, int n){
+		/*implementation not shown*/
+	}
+	/** Modifies the current phrase by replacing the nth occurrence of str with repl.
+	* If the nth occurrence does not exist, the current phrase is unchanged.
+	* Precondition: str.length() > 0 and N > 0
+	*/
 	public void replaceNthOccurrence(String str, int n, String repl){
 		int index = findNthOccurrence(str, n);
 		if (index>-1){
@@ -32,11 +32,11 @@ public class Phrase{
 			currentPhrase = beginning+repl+end;
 		}
 	}
-  /** Returns the index of the last occurrence of str in the current phrase;
-  * returns -1 if str is not found.
-  * Precondition: str.length() > 0
-  * Postcondition: the current phrase is not modified.
-  */
+	/** Returns the index of the last occurrence of str in the current phrase;
+	* returns -1 if str is not found.
+	* Precondition: str.length() > 0
+	* Postcondition: the current phrase is not modified.
+	*/
 	public int findLastOccurence (String str){
 		int i = 0;
 		while (findNthOccurrence(str,i+1)!=-1){
@@ -44,6 +44,7 @@ public class Phrase{
 		}
 		return findNthOccurrence(str,n);
 	}
+	/** Returns a string containing the current phrase */
 	public String toString(){
 		return currentPhrase;
 	}
